92007e713d3a9c66bd5c8d36c44284a4639f3c73
Gabriela Barrera <g.barrera.angel@gmail.com>
2020-03-03 18:35:26 -0300

    Creación del componente "mi-componente"

---
+++ src/app/components/mi-componente/mi-componente.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'mi-componente', //etiqueta html
+    template: `
+        
+        <h1> {{titulo}} </h1>
+        <p> {{ comentario }} </p>
+        <h2>{{ year }}</h2>
+    `
+})
+
+export class MiComponente {
+
+    //propiedades
+    public titulo: string;
+    public comentario: string;
+    public year: number;
+    
+
+    constructor(){
+        this.titulo = "Hola mundo, soy Mi-Componente";
+        this.comentario = "Este es mi primer componente";
+        this.year = 2010;
+
+        console.log("Componente mi-componente cargado!!");
+        console.log(this.titulo, this.comentario, this.year);
+    }
+    
+}
